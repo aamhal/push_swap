323fb304b928e64c1a8267d40d3737ca728e180c
Amhal Abdelouhab <116556453+aamhal@users.noreply.github.com>
2023-06-05 08:23:37 +0100

    Add files via upload

--- push_swap/SRC/main.c
+++ push_swap/SRC/main.c
@@ -6,7 +6,7 @@
 /*   By: aamhal <aamhal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 11:53:02 by aamhal            #+#    #+#             */
-/*   Updated: 2023/06/04 17:49:51 by aamhal           ###   ########.fr       */
+/*   Updated: 2023/06/04 21:22:36 by aamhal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,21 @@ int	main(int ac, char **av)
 	p = parsing(ac, av);
 	tmp = char_int(p);
 	a = fill_stack(tmp, p);
+	while (p[i])
+		i++;
 	find_index(&a);
 	nbr_position(&a);
-	if (ac == 4)
-	{
+	if (i == 3)
 		sort_3_nbr(&a);
-	}
-	else if (ac == 5)
+	else if (i == 4)
 		sort_4_nbr(&a, &b);
-	else if (ac == 6)
+	else if (i == 5)
 		sort_5_nbr(&a, &b);
 	else
 	{
-	fill_stack_b(&a,&b);
-	nbr_position(&b);
-	fill_stack_a(&a,&b);
+		fill_stack_b(&a,&b);
+		nbr_position(&b);
+		fill_stack_a(&a,&b);
 	}
-
 	return (0);
 }
